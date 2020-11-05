e597b5f1e4b6170d6e653c165d29279af6005279
Simon Kjellgren <simon.kjellgren@learnet.se>
2020-11-05 11:08:59 +0100

    Begin making the foundation of the game

---
+++ Platformer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Platformer
+{
+    class Program
+    {
+        static void Main()
+        {
+            int DisplayWidth = 1920;
+            int DisplayHeight = 1080;
+
+            Raylib.InitWindow(DisplayWidth, DisplayHeight, "Platformer game");
+            Raylib.ToggleFullscreen();
+
+            bool closeMainMenu = false;
+            bool displayRestart = false;
+            bool pressToRestart = false;
+            int level = 0;
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawRectangle(10, 10, 100, 100, Color.ORANGE);
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
